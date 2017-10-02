616ae2d55b1c68f26a376d0ed8aac48df7f2b6ba
Williammer <supervegeter@gmail.com>
2017-10-02 22:51:21 +0800

    refactor on the language level

--- src/meteorShower.js
+++ src/meteorShower.js
@@ -1,56 +1,65 @@
-class Crood {
+function rand(max) {
+  return Math.random() * max;
+}
+
+class Coordinate {
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
   }
-  setCrood(x, y) {
+
+  setCoordinate(x, y) {
     this.x = x;
     this.y = y;
   }
 }
 
 class Meteor {
-  constructor(init = new Crood(), final = new Crood(), size = 3, speed = 200, onDestroy = null) {
-    this.init = init; // 初始位置
-    this.final = final; // 最终位置
-    this.size = size; // 大小
-    this.speed = speed; // 速度：像素/s
+  constructor(init = new Coordinate(), final = new Coordinate(), size = 3, speed = 200, onDestroy = null) {
+    this.init = init;
+    this.final = final;
+    this.size = size;
+    this.speed = speed; // px/s
 
-    // 飞行总时间
-    this.dur = Math.sqrt(Math.pow(this.final.x - this.init.x, 2) + Math.pow(this.final.y - this.init.y, 2)) * 1000 / this.speed;
+    this.dur = Math.sqrt((this.final.x - this.init.x) ** 2 + (this.final.y - this.init.y) ** 2) * 1000 / this.speed;
+    this.passed = 0;
 
-    this.pass = 0; // 已过去的时间
-    this.prev = this.init.copy(); // 上一帧位置
-    this.now = this.init.copy(); // 当前位置
+    this.prev = new Coordinate(this.init.x, this.init.y);
+    this.now = new Coordinate(this.init.x, this.init.y);
+
     this.onDestroy = onDestroy;
   }
+
   draw(ctx, delta) {
-    this.pass += delta;
-    this.pass = Math.min(this.pass, this.dur);
+    this.passed += delta;
+    this.passed = Math.min(this.passed, this.dur);
 
-    const percent = this.pass / this.dur;
+    const percent = this.passed / this.dur;
 
-    this.now.setCrood(
+    this.now.setCoordinate(
       this.init.x + (this.final.x - this.init.x) * percent,
       this.init.y + (this.final.y - this.init.y) * percent,
     );
 
-    // canvas
+    // draw on canvas
     ctx.strokeStyle = '#fff';
     ctx.lineCap = 'round';
     ctx.lineWidth = this.size;
+
     ctx.beginPath();
     ctx.moveTo(this.now.x, this.now.y);
     ctx.lineTo(this.prev.x, this.prev.y);
     ctx.stroke();
 
-    this.prev.setCrood(this.now.x, this.now.y);
-    if (this.pass === this.dur) {
+    this.prev.setCoordinate(this.now.x, this.now.y);
+
+    if (this.passed === this.dur) {
       this.destroy();
     }
   }
+
   destroy() {
-    this.onDestroy && this.onDestroy();
+    this.onDestroy && this.onDestroy(this);
   }
 }
 
@@ -59,54 +68,53 @@ class MeteorShower {
   constructor(cvs, ctx, config = {}) {
     this.cvs = cvs;
     this.ctx = ctx;
-    this.stars = [];
-    this.T;
+    this.meteors = [];
+    this.animation;
     this.stop = false;
-    this.playing = false;
 
-    this._starCount = config.starCount;
+    this.meteorCount = config.meteorCount;
+    this.maxPathHeight = config.maxPathHeight;
+    this.maxPathLength = config.maxPathLength;
+    this.maxSize = config.maxSize;
+    this.minSpeed = config.minSpeed;
+    this.maxSpeed = config.maxSpeed;
   }
 
-  createStar() {
-    const angle = Math.PI * Math.random();
-    const distance = Math.random() * 600;
-    const init = new Crood(Math.random() * this.cvs.width | 0, Math.random() * 300 | 0);
-    const final = new Crood(init.x + distance * Math.cos(angle), init.y + distance * Math.sin(angle));
-    const size = Math.random() * 2;
-    const speed = Math.random() * 500 + 300;
+  createMeteor() {
+    const angle = rand(Math.PI);
+    const pathLength = rand(this.maxPathLength);
+    const init = new Coordinate(rand(this.cvs.width), rand(this.maxPathHeight));
+    const final = new Coordinate(init.x + pathLength * Math.cos(angle), init.y + pathLength * Math.sin(angle));
+    const size = rand(this.maxSize);
+    const speed = Math.max(rand(this.maxSpeed), this.minSpeed);
 
-    const star = new Meteor(
+    return new Meteor(
       init, final, size, speed,
-      () => { this.remove(star); },
+      this.removeMeteor.bind(this),
     );
-    return star;
   }
 
-  remove(star) {
-    this.stars = this.stars.filter(s => s !== star);
+  removeMeteor(meteor) {
+    this.meteors = this.meteors.filter(s => s !== meteor); // no way its deep equal
   }
 
   update(delta) {
-    if (!this.stop && this.stars.length < this._starCount) {
-      this.stars.push(this.createStar());
+    if (!this.stop && this.meteors.length < this.meteorCount) {
+      this.meteors.push(this.createMeteor());
     }
-    this.stars.forEach((star) => {
-      star.draw(this.ctx, delta);
+    this.meteors.forEach((meteor) => {
+      meteor.draw(this.ctx, delta);
     });
   }
 
   tick() {
-    if (this.playing) return;
-    this.playing = true;
-
     const now = (new Date()).getTime();
     let last = now;
     let delta;
 
     const _tick = () => {
-      if (this.stop && this.stars.length === 0) {
-        window.cancelAnimationFrame(this.T);
-        this.playing = false;
+      if (this.stop && this.meteors.length === 0) {
+        window.cancelAnimationFrame(this.animation);
         return;
       }
 
@@ -114,7 +122,7 @@ class MeteorShower {
       delta = delta > 500 ? 30 : (delta < 16 ? 16 : delta);
       last = now;
 
-      this.T = window.requestAnimationFrame(_tick);
+      this.animation = window.requestAnimationFrame(_tick);
 
       this.ctx.save();
 
@@ -125,6 +133,7 @@ class MeteorShower {
       this.ctx.restore();
       this.update(delta);
     };
+
     _tick();
   }
 
@@ -142,6 +151,11 @@ const canvas = document.querySelector('canvas');
 const ctx2d = canvas.getContext('2d');
 
 new MeteorShower(canvas, ctx2d, {
-  starCount: 1,
+  meteorCount: 1,
+  maxPathHeight: 500,
+  maxPathLength: 600,
+  maxSize: 3,
+  minSpeed: 500,
+  maxSpeed: 1000,
 })
   .start();
