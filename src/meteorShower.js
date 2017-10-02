783eb8eb6e073c043e672fa7fea7f44307954437
Williammer <supervegeter@gmail.com>
2017-10-02 11:08:35 +0800

    Update shower

---
+++ src/meteorShower.js
@@ -0,0 +1,151 @@
+class Crood {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  setCrood(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  copy() {
+    return new Crood(this.x, this.y);
+  }
+}
+
+class Meteor {
+  constructor(init = new Crood(), final = new Crood(), size = 3, speed = 200, onDistory = null) {
+    this.init = init; // 初始位置
+    this.final = final; // 最终位置
+    this.size = size; // 大小
+    this.speed = speed; // 速度：像素/s
+
+    // 飞行总时间
+    this.dur = Math.sqrt(Math.pow(this.final.x - this.init.x, 2) + Math.pow(this.final.y - this.init.y, 2)) * 1000 / this.speed;
+
+    this.pass = 0; // 已过去的时间
+    this.prev = this.init.copy(); // 上一帧位置
+    this.now = this.init.copy(); // 当前位置
+    this.onDistory = onDistory;
+  }
+  draw(ctx, delta) {
+    this.pass += delta;
+    this.pass = Math.min(this.pass, this.dur);
+
+    const percent = this.pass / this.dur;
+
+    this.now.setCrood(
+      this.init.x + (this.final.x - this.init.x) * percent,
+      this.init.y + (this.final.y - this.init.y) * percent,
+    );
+
+    // canvas
+    ctx.strokeStyle = '#fff';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = this.size;
+    ctx.beginPath();
+    ctx.moveTo(this.now.x, this.now.y);
+    ctx.lineTo(this.prev.x, this.prev.y);
+    ctx.stroke();
+
+    this.prev.setCrood(this.now.x, this.now.y);
+    if (this.pass === this.dur) {
+      this.distory();
+    }
+  }
+  distory() {
+    this.onDistory && this.onDistory();
+  }
+}
+
+
+class MeteorShower {
+  constructor(cvs, ctx, config = {}) {
+    this.cvs = cvs;
+    this.ctx = ctx;
+    this.stars = [];
+    this.T;
+    this.stop = false;
+    this.playing = false;
+
+    this._starCount = config.starCount;
+  }
+
+  createStar() {
+    const angle = Math.PI * Math.random();
+    const distance = Math.random() * 600;
+    const init = new Crood(Math.random() * this.cvs.width | 0, Math.random() * 300 | 0);
+    const final = new Crood(init.x + distance * Math.cos(angle), init.y + distance * Math.sin(angle));
+    const size = Math.random() * 2;
+    const speed = Math.random() * 500 + 300;
+
+    const star = new Meteor(
+      init, final, size, speed,
+      () => { this.remove(star); },
+    );
+    return star;
+  }
+
+  remove(star) {
+    this.stars = this.stars.filter(s => s !== star);
+  }
+
+  update(delta) {
+    if (!this.stop && this.stars.length < this._starCount) {
+      this.stars.push(this.createStar());
+    }
+    this.stars.forEach((star) => {
+      star.draw(this.ctx, delta);
+    });
+  }
+
+  tick() {
+    if (this.playing) return;
+    this.playing = true;
+
+    const now = (new Date()).getTime();
+    let last = now;
+    let delta;
+
+    const _tick = () => {
+      if (this.stop && this.stars.length === 0) {
+        cancelAnimationFrame(this.T);
+        this.playing = false;
+        return;
+      }
+
+      delta = now - last;
+      delta = delta > 500 ? 30 : (delta < 16 ? 16 : delta);
+      last = now;
+
+      this.T = requestAnimationFrame(_tick);
+
+      ctx.save();
+
+      ctx.fillStyle = 'rgba(0,0,0,0.9)';
+      ctx.globalCompositeOperation = 'destination-in';
+
+      ctx.fillRect(0, 0, cvs.width, cvs.height);
+      ctx.restore();
+      this.update(delta);
+    };
+    _tick();
+  }
+
+  start() {
+    this.stop = false;
+    this.tick();
+  }
+
+  stop() {
+    this.stop = true;
+  }
+}
+
+const cvs = document.querySelector('canvas');
+const ctx = cvs.getContext('2d');
+
+const meteorShower = new MeteorShower(cvs, ctx, {
+  starCount: 1,
+});
+
+meteorShower.start();
